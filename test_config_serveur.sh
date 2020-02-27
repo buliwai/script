c99ea7ea1573c4fbb6f187d8f40c9eb31a7ac0b5
root <root@vps785432.ovh.net>
2020-02-27 16:16:18 +0100

    dossier script

---
+++ test_config_serveur.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROJET="toto"
+
+echo $PROJET
+
+apt install mariadb-server mariadb-client
+
+apt install apache2 libapache2-mod-php7.0
+
+mkdir -p /test_config
+mkdir -p /test_config
+
+
+
